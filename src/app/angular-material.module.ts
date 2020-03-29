4f21feba16ec03323837fb9e633325b417502c22
josepereza <josepereza66@gmail.com>
2020-03-29 11:15:55 +0200

    first-3 commit

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -4,6 +4,7 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
@@ -12,14 +13,16 @@ import {MatButtonModule} from '@angular/material/button';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   exports:[
     CommonModule,
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
 })
 export class AngularMaterialModule { }
